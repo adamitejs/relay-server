6a7aad2a34726a7fe4efb9e1f4aaf2b6dffb7223
Jesse Dunlap <jesse@hydrais.com>
2019-11-22 13:39:55 -0600

    Repo reset

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,13 +1,5 @@
-import index from "../src/index";
-import RelayServer from "../src/RelayServer";
-jest.mock("../src/RelayServer");
-
 describe("index", () => {
-  it("should return an RelayServer instance", () => {
-    const mockRelayConfig = { "123": "abc" };
-    const mockAdamiteConfig = { abc: "123" };
-    const server = index(mockRelayConfig, mockAdamiteConfig);
-    expect(RelayServer).toHaveBeenCalledWith(mockRelayConfig, mockAdamiteConfig);
-    expect(server).toBe((RelayServer as any).mock.instances[0]);
+  it("should run", () => {
+    expect(1).toBe(1);
   });
 });
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import RelayServer from "./RelayServer";
-
-export default function(relayConfig: any, serviceConfig: any, adamiteConfig: any): RelayServer {
-  return new RelayServer(relayConfig, serviceConfig, adamiteConfig);
-}
